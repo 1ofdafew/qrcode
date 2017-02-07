f7b771ae79244ea608e5d937e76dbb0825fddbc2
Da Few <1ofdafew@gmail.com>
2017-02-07 17:26:41 +0800

    Removed ebin, and updated .d file;

--- qrcode.d
+++ qrcode.d
@@ -1,6 +1,6 @@
-src/qrcode.erl:: include/qrcode.hrl src/qrcode_params.hrl; @touch $@
+src/qrcode.erl:: include/qrcode.hrl include/qrcode_params.hrl; @touch $@
 src/qrcode_demo.erl:: include/qrcode.hrl; @touch $@
-src/qrcode_mask.erl:: src/qrcode_params.hrl; @touch $@
-src/qrcode_matrix.erl:: src/qrcode_params.hrl; @touch $@
+src/qrcode_mask.erl:: include/qrcode_params.hrl; @touch $@
+src/qrcode_matrix.erl:: include/qrcode_params.hrl; @touch $@
 
 COMPILE_FIRST +=
